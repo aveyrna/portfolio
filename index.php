151ed83e85d09f45aab4b5f455de823be67d71fc
0blivi0n <aubry.varen@gmail.com>
2026-01-09 15:57:19 +0100

    Adding Background

--- index.php
+++ index.php
@@ -128,37 +128,109 @@
     </section>
 
     <section id="background">
-        <h2>Parcours</h2>
+        <h2>Mon parcours (formations et expériences)</h2>
 
+        <h3>Juin - Décembre 2025 : Coordinateur technique et pédagogique</h3>
+        <h4>Efficenty - Saint-Ouen-Sur-Seine</h4>
         <p>
-            Mon parcours n’a pas été linéaire.
-            Il a été construit par l’expérimentation, les essais réels,
-            et parfois les remises en question nécessaires.
-            J’ai été formé à Zone01, une école d’informatique atypique
-            basée sur l’apprentissage par la pratique, le travail en autonomie et la collaboration.
-            Pas de cours magistraux, pas de recettes toutes faites : on apprend en codant,
-            en se trompant, en corrigeant.
-            Cette approche m’a appris à raisonner, à chercher par moi-même
-            et à comprendre ce que je fais, plutôt que d’empiler des frameworks.
-            <br /><br />
-            Au fil de cette formation, j’ai réalisé plusieurs stages et projets concrets,
-            orientés back-end et outils. J’y ai travaillé sur des bases de données, des APIs,
-            des architectures simples mais solides.
-            C’est là que s’est ancrée une chose importante pour moi : le code n’est pas une fin en soi.
-            C’est un moyen de structurer des idées et de rendre des systèmes compréhensibles dans le temps.
-            Par la suite, j’ai travaillé en tant que coach pour Efficenty et Zone01.
-            J’y ai accompagné des apprenants dans leur montée en compétence : déblocage technique, méthodologie,
-            mais aussi posture face à la difficulté. Ce rôle m’a appris à expliquer clairement, à écouter,
-            et surtout à respecter les rythmes d’apprentissage.
-            Encadrer m’a forcé à clarifier ma propre compréhension.
-            <br /><br />
-            Avec le temps, j’ai compris que si j’aimais transmettre, je ne voulais pas en faire mon activité principale.
-            J’ai choisi de revenir à la création : construire mes propres outils, écrire mes propres univers, avancer à mon rythme.
-            Cette expérience de coach reste pourtant centrale dans ma manière de penser :
-            elle m’a appris la rigueur, la pédagogie, et une forme de responsabilité dans ce que je produis.
-            <br /><br />
-            Aujourd’hui, ce parcours nourrit directement ce que je crée : des projets techniques sobres,
-            des outils pensés pour durer, et des récits ancrés dans des choix humains plutôt que dans des effets de surface.
+            En alternance de ma formation, j'ai intégré l'équipe pédagogique de Efficenty en tant que coordinateur technique et pédagogique.
+            J'ai eu pour mission d'accompagner les étudiants dans leur apprentissage,
+            de les encadrer dans leurs projets en les guidant dans un parcours du même type que celui que j'avais découvert à Zone01.
+
+            Cette expérience m'a permis de développer mes compétences en communication et en encadrement,
+            tout en consolidant mes connaissances techniques. Mais bien que cette expérience ait été enrichissante,
+            j'ai ressenti le besoin de me concentrer pleinement sur mes propres projets de création et sur le développement.
+
+            Si l'encadrement d'étudiants était gratifiant, ce n'est pas ce que je cherchais à long terme.
+        </p>
+
+        <h3>2023 - 2025 : Formation Développeur fullstack (Bac+3)</h3>
+        <h4>Zone01 - Rouen</h4>
+        <p>
+            C'est en octobre 2022 que j'ai entendu parler de zone01 par un conseiller en insertion professionnelle qui prenait enfin en compte mes motivations.
+            Après les tests en ligne, comme pour 42, j'ai intégré la piscine de séléction. Un nouveau mois d'épreuves intensives, en Go cette fois.
+            J'ai cette fois été retenu pour intégrer la formation de Développeur fullstack en mai 2023.<br><br>
+            La formation couvre un large éventail de technologies: Golang, HTML/CSS, JavaScript, Rust, bases de données (SQLite, PostgreSQL), et plus encore.
+            Après deux ans de formation, j'ai acquis les compétences que je cherchais depuis longtemps.
+        </p>
+
+        <h3>2021 - 2022 : Programme en ligne Unity Learn</h3>
+        <h4>Unity Technologies</h4>
+        <p>
+            J'ai suivi plusieurs parcours de formation en ligne proposés par Unity Technologies,
+            couvrant les bases du développement de jeux vidéo avec le moteur Unity.
+            J'y ai appris le C#, la gestion des assets, la physique dans les jeux, et les principes fondamentaux du game design.
+            Cette formation m'a permis de créer mes premiers prototypes de jeux vidéo.
+
+            18 fevrier 2021 - Complétion du parcours "Unity Essentials"
+
+            Puis poursuite avec le parcours "Unity Junior Programmer" que je finirai le 28 janvier 2023, quelques semaines avant de commencer ma formation fullstack.
+        </p>
+
+        <h3>2019 - 2020 : Piscine 42 et passage à vide</h3>
+        <h4>École 42 - Paris</h4>
+        <p>
+            J'ai intégré la piscine de l'école 42 à Paris pour un mois d'épreuves intensives afin d'intégrer la formation.
+            Cependant, bien qu'étant arrivé au bout de la piscine et avoir obtenu un score correct à l'examen final (55%), je n'ai pas été retenu. <br><br>
+            Cette expérience m'a beaucoup appris sur la programmation en C et sur ce qu'est la programmation.
+            De nombreuses erreurs à régler, encore et encore, necessitant patience et persévérance.<br><br>
+            Après cette expérience, je savais que je voulais continuer à apprendre le développement,
+            mais mes expériences passées ne semblaient pas suffisantes à mes conseillers pour me diriger vers des formations plus avancées.
+            J'ai donc recommencé à apprendre de mon côté, en autodidacte. Sur des domaines plus poussés cette fois.
+        </p>
+
+        <h3>2018 : Formation Développeur intégrateur en développement d'applications web (Bac+2)</h3>
+        <h4>OpenClassrooms & 3W Academy</h4>
+        <p>
+            Mon premier vrai pas dans le développement informatique.<br>
+            J'ai suivi une formation à distance de 6 mois en développement web full-stack,
+            couvrant HTML, CSS, JavaScript, PHP et MySQL.
+            J'y ai décroché mon premier titre professionnel de Développeur intégrateur d'applications web.
+        </p>
+
+        <h3>2016 - 2017 : Première expérience autonome - Rédacteur web</h3>
+        <h4>Indépendant</h4>
+        <p>
+            Je ne parle pas ici de Développement web car j'utilisais alors le CMS Wordpress.<br><br>
+            Ce fut ma première expérience en tant qu'indépendant,
+            principalement en tant que rédacteur web pour mon propre site de l'époque (Youmad.fr).
+            Le site traitant du jeu vidéo et de la culture numérique dans son ensemble,
+            j'ai tiré de cette expérience d'importantes leçons sur la veille informatique et la création de contenu.
+            Je me déplaçais souvent dans les salons et événements liés au jeu vidéo pour y couvrir l'actualité.<br><br>
+            Ne parvenant pas à en générer des revenus suffisants, j'ai décidé de continuer mon apprentissage.
+        </p>
+
+        <h3>2013 - 2015 : Activités diverses et préparation à l'informatique</h3>
+        <h4>Paris-Normandie / AFPA</h4>
+        <p>
+            Durant cette période, j'ai occupé divers emplois temporaires,
+            notamment en tant que correspondant de presse chez Paris-Normandie.
+            Parallèlement, j'ai suivi une formation de quelques mois dans l'hotellerie-restauration avec l'AFPA,
+            durant laquelle j'ai effectué un stage de 3 semaines en tant que récéptionniste au Village Club du Soleil Les Karellis.
+            Je commençais en parrallèle à me former en autodidacte à l'informatique et au développement web.
+        </p>
+
+        <h3>2011 - 2012 : Agent de production en industrie pharmaceutique</h3>
+        <h4>Fresenius Kabi France - Louviers</h4>
+        <p>
+            De retour dans l'industrie pharmaceutique, j'ai travaillé sur la production de solutions
+            intraveineuses. Cette expérience m'a permis de continuer à gagner ma vie le temps de définir ma poursuite d'études.
+        </p>
+
+        <h3>2011: Diplôme d'Accès aux Études Universitaires (DAEU) — Série A (Littéraire)</h3>
+        <h4>-Université d'Orléans - Antenne de Chateauroux</h4>
+        <p>
+            J'ai décidé de reprendre mes études en visant un diplôme équivalent au baccalauréat.
+            Le DAEU m'a permis d'acquérir des bases solides en littérature, philosophie et langues,
+            tout en développant ma capacité de travail autonome.
+        </p>
+
+        <h3>2005 - 2011 : Agent de production en industrie pharmaceutique</h3>
+        <h4>-GlaxoSmithKline - Evreux</h4>
+        <p>Mon premier emploi, à une époque où je n'avais aucun diplôme en poche.
+            Je travaillais sur les chaînes de conditionnement des inhalateurs Diskus.
+            J'y ai appris la rigueur, le travail en équipe et le sens des responsabilités.
+            Mais après plusieurs années, j'ai ressenti le besoin de me réorienter.
         </p>
 
     </section>
